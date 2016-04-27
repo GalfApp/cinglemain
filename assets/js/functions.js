737c97b1d4d7a3bb20b2be05f22addc294f98a1e
duvan <duvanvargas@galf.com>
2016-04-27 15:10:16 -0500

    fix1

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,6 +7,26 @@ Author URL: jhonmo09@gmail.com/
 Author: Jhon Moreno
 Author URL: jhonmo09@gmail.com/
 */
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+var tech = getUrlParameter('terms');
+if (tech=="open") {
+  //Abrir modal de terminos
+  console.log("Abriendo modal");
+  openTerms();
+}
 
 function cancelEvent(event) {
    if (event.preventDefault) { 
